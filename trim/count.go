1a8b1be78efa4b6028a17089153daeae9bc5bd42
Nikolay Petrov <sungon@gmail.com>
2023-07-29 17:03:19 -0400

    add trim by count

---
+++ trim/count.go
@@ -0,0 +1,52 @@
+package trim
+
+import (
+	"context"
+
+	"github.com/klev-dev/klevdb"
+)
+
+func ByCount(ctx context.Context, l klevdb.Log, max int64) (map[int64]struct{}, int64, error) {
+	stats, err := l.Stat()
+	switch {
+	case err != nil:
+		return nil, 0, err
+	case int64(stats.Messages) <= max:
+		return nil, 0, nil
+	}
+
+	maxOffset, err := l.NextOffset()
+	if err != nil {
+		return nil, 0, err
+	}
+
+	var deleteOffsets = map[int64]struct{}{}
+
+	toRemove := int64(stats.Messages) - max
+	for offset := klevdb.OffsetOldest; offset < maxOffset && toRemove > 0; {
+		nextOffset, msgs, err := l.Consume(offset, 32)
+		if err != nil {
+			return nil, 0, err
+		}
+		offset = nextOffset
+
+		for _, msg := range msgs {
+			deleteOffsets[msg.Offset] = struct{}{}
+			toRemove--
+
+			if toRemove == 0 {
+				break
+			}
+		}
+
+		if err := ctx.Err(); err != nil {
+			return nil, 0, err
+		}
+	}
+
+	if err := ctx.Err(); err != nil {
+		return nil, 0, err
+	}
+
+	return l.Delete(deleteOffsets)
+}
--- trim/count_test.go
+++ trim/count_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestBySize(t *testing.T) {
+func TestByCount(t *testing.T) {
 	msgs := message.Gen(20)
 
 	l, err := klevdb.Open(t.TempDir(), klevdb.Options{})
@@ -21,21 +21,21 @@ func TestBySize(t *testing.T) {
 
 	stat, err := l.Stat()
 	require.NoError(t, err)
-	require.Equal(t, l.Size(msgs[0])*20, stat.Size)
+	require.Equal(t, len(msgs), stat.Messages)
 
 	msg, err := l.Get(klevdb.OffsetOldest)
 	require.NoError(t, err)
 	require.Equal(t, int64(0), msg.Offset)
 
 	t.Run("None", func(t *testing.T) {
-		off, sz, err := BySize(context.TODO(), l, l.Size(msgs[0])*21)
+		off, sz, err := ByCount(context.TODO(), l, 21)
 		require.Len(t, off, 0)
 		require.NoError(t, err)
 		require.Equal(t, int64(0), sz)
 
 		stat, err = l.Stat()
 		require.NoError(t, err)
-		require.Equal(t, l.Size(msgs[0])*20, stat.Size)
+		require.Equal(t, len(msgs), stat.Messages)
 
 		msg, err = l.Get(klevdb.OffsetOldest)
 		require.NoError(t, err)
@@ -43,15 +43,14 @@ func TestBySize(t *testing.T) {
 	})
 
 	t.Run("Half", func(t *testing.T) {
-		toTrimSize := l.Size(msgs[0]) * 11
-		off, sz, err := BySize(context.TODO(), l, toTrimSize)
+		off, sz, err := ByCount(context.TODO(), l, 10)
 		require.Len(t, off, 10)
 		require.NoError(t, err)
 		require.Equal(t, l.Size(msgs[0])*10, sz)
 
 		stat, err = l.Stat()
 		require.NoError(t, err)
-		require.Equal(t, l.Size(msgs[0])*10, stat.Size)
+		require.Equal(t, 10, stat.Messages)
 
 		msg, err = l.Get(klevdb.OffsetOldest)
 		require.NoError(t, err)
