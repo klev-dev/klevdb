1c609624ab9c59d3960d7c3851ab31d55370bc10
Nikolay Petrov <sungon@gmail.com>
2023-10-07 15:54:03 -0400

    support relative offsets in trim by offset

--- trim/offset.go
+++ trim/offset.go
@@ -4,16 +4,23 @@ import (
 	"context"
 
 	"github.com/klev-dev/klevdb"
+	"github.com/klev-dev/klevdb/message"
 )
 
 // ByOffset tries to remove the messages at the start of the log before offset
 // returns the offsets it deleted and the amount of storage freed
 func ByOffset(ctx context.Context, l klevdb.Log, before int64) (map[int64]struct{}, int64, error) {
+	if before == message.OffsetOldest {
+		return map[int64]struct{}{}, 0, nil
+	}
+
 	maxOffset, err := l.NextOffset()
 	if err != nil {
 		return nil, 0, err
 	}
-	if maxOffset > before {
+	if before == message.OffsetNewest {
+		before = maxOffset
+	} else if maxOffset > before {
 		maxOffset = before
 	}
 
--- trim/offset_test.go
+++ trim/offset_test.go
@@ -71,3 +71,51 @@ func TestByOffset(t *testing.T) {
 		require.ErrorIs(t, err, message.ErrInvalidOffset)
 	})
 }
+
+func TestByOffsetRelative(t *testing.T) {
+	msgs := message.Gen(20)
+
+	l, err := klevdb.Open(t.TempDir(), klevdb.Options{})
+	require.NoError(t, err)
+	defer l.Close()
+
+	_, err = l.Publish(msgs)
+	require.NoError(t, err)
+
+	stat, err := l.Stat()
+	require.NoError(t, err)
+	require.Equal(t, len(msgs), stat.Messages)
+
+	msg, err := l.Get(klevdb.OffsetOldest)
+	require.NoError(t, err)
+	require.Equal(t, int64(0), msg.Offset)
+
+	t.Run("Oldest", func(t *testing.T) {
+		off, sz, err := ByOffset(context.TODO(), l, message.OffsetOldest)
+		require.Len(t, off, 0)
+		require.NoError(t, err)
+		require.Equal(t, int64(0), sz)
+
+		stat, err = l.Stat()
+		require.NoError(t, err)
+		require.Equal(t, len(msgs), stat.Messages)
+
+		msg, err = l.Get(klevdb.OffsetOldest)
+		require.NoError(t, err)
+		require.Equal(t, int64(0), msg.Offset)
+	})
+
+	t.Run("Newest", func(t *testing.T) {
+		off, sz, err := ByOffset(context.TODO(), l, message.OffsetNewest)
+		require.Len(t, off, 20)
+		require.NoError(t, err)
+		require.Equal(t, l.Size(msgs[0])*20, sz)
+
+		stat, err = l.Stat()
+		require.NoError(t, err)
+		require.Equal(t, 0, stat.Messages)
+
+		msg, err = l.Get(klevdb.OffsetOldest)
+		require.ErrorIs(t, err, message.ErrInvalidOffset)
+	})
+}
