02682d1e36d3aac2421c7e9eb9423a542e4c90a2
Nikolay Petrov <sungon@gmail.com>
2024-10-20 20:27:04 -0400

    add nix flake/direnv

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "klevdb is a log storage db";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+          ];
+        };
+      });
+}
