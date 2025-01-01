32f0be6aed0460a1d7a51245272aa77422ef5992
Nikolay Petrov <sungon@gmail.com>
2025-01-01 11:04:44 -0500

    go fmt

--- delete.go
+++ delete.go
@@ -30,11 +30,16 @@ func DeleteMultiWithWait(d time.Duration) DeleteMultiBackoff {
 }
 
 // DeleteMulti tries to delete all messages with offsets
-//   from the log and returns the amount of storage deleted
+//
+//	from the log and returns the amount of storage deleted
+//
 // If error is encountered, it will return the deleted offsets
-//   and size, together with the error
+//
+//	and size, together with the error
+//
 // DeleteMultiBackoff is called on each iteration to give
-//   others a chanse to work with the log, while being deleted
+//
+//	others a chanse to work with the log, while being deleted
 func DeleteMulti(ctx context.Context, l Log, offsets map[int64]struct{}, backoff DeleteMultiBackoff) (map[int64]struct{}, int64, error) {
 	var deletedOffsets = map[int64]struct{}{}
 	var deletedSize int64
