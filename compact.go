3e9d428e641a37a32da587cf2171817c9089e5ae
Nikolay Petrov <sungon@gmail.com>
2026-04-20 21:10:26 -0400

    unified compaction

---
+++ compact.go
@@ -0,0 +1,18 @@
+package klevdb
+
+import (
+	"context"
+	"time"
+)
+
+func Compact(ctx context.Context, l Log, age time.Duration, boff DeleteMultiBackoff) error {
+	updatesBefore := time.Now().Add(-age)
+	if _, _, err := CompactUpdatesMulti(ctx, l, updatesBefore, boff); err != nil {
+		return err
+	}
+	deletesBefore := time.Now().Add(-age * 2)
+	if _, _, err := CompactDeletesMulti(ctx, l, deletesBefore, boff); err != nil {
+		return err
+	}
+	return l.GC(0)
+}
